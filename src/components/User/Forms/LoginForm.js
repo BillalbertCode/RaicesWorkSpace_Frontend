e39e7457a8a8d4d362b5af60944d44146b6832ae
BillalbertCode <BillalbertCode@gmail.com>
2024-04-29 17:42:53 -0400

    LoginformMini

--- src/components/User/Forms/LoginForm.js
+++ src/components/User/Forms/LoginForm.js
@@ -52,7 +52,7 @@ const LoginForm = () => {
                 <button type="submit" className="btn btn-outline-primary btn-sm mb-2">Iniciar Sesion</button>
 
                 <Link href="/register" className="icon-link icon-link-hover link-body-emphasis link-underline-opacity-0 mb-2">Registrarse
-                    <svg xmlns="http://www.w3.org/2000/svg" aria-hidden="true" width="16" height="16" fill="currentColor" class="bi bi-arrow-right" viewBox="0 0 16 16">
+                    <svg xmlns="http://www.w3.org/2000/svg" aria-hidden="true" width="16" height="16" fill="currentColor" className="bi bi-arrow-right" viewBox="0 0 16 16">
                         <path fill-rule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8" />
                     </svg>
                 </Link>
