a6379cb7864e038a2c5a22abb6e0f80a82570442
BillalbertCode <BillalbertCode@gmail.com>
2024-06-02 18:07:55 -0400

    Design Navbar cuadroBg- Animation + formato del cuadro

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,14 +1,25 @@
-import { useContext } from "react";
 import Link from "next/link";
+// hooks
+import { useState, useEffect } from "react";
+// Context
+import { useContext } from "react";
 import { TokenContext } from "@/contexts/TokenContext";
+// Componets
 import LoginModal from "../utils/LoginModal";
+// Styles
+import stylesNavbar from "../../styles/navbar.module.css"
+
+
 
 const Header = () => {
   const { loginStatus, closedSession } = useContext(TokenContext)
 
+
   return (
-    <nav className="navbar navbar-expand-lg bg-body-tertiary">
-      <div className="container-fluid">
+    <nav className="navbar navbar-expand-lg bg-transparent position-relative overflow-hidden">
+      <div className={`${stylesNavbar.bgAnimated}`}></div>
+      <div className="container-fluid ">
+      <button className="btn btn-success" onClick={createCuadro}>Crear cuadro</button>
         <Link href="/" className="navbar-brand" >Raices</Link>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
@@ -18,7 +29,7 @@ const Header = () => {
             <Link href="/" className="nav-link ">Home</Link>
 
             {/* Accion segun el estatus de inicio de sesion */}
-            
+
             {loginStatus
               ? (<>
                 <Link href="/profile" className="nav-link"> Profile</Link>
